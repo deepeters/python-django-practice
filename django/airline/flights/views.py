58dadf51f34603c93aae5538833187910b162545
Dennis Peters <dennis95peters@gmail.com>
2021-08-15 10:33:30 +0300

    add passangers to flight

--- django/airline/flights/views.py
+++ django/airline/flights/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-from .models import Flight
+from .models import Flight, Passenger
 
 # Create your views here.
 def index(request): 
     return render(request, "flights/index.html", {
         "flights": Flight.objects.all()
+    })
+
+def flight(request, flight_id):
+    flight = Flight.objects.get(pk=flight_id)
+    return render(request, "flights/flight.html", {
+        "flight": flight,
+        "passengers":flight.passenger.all()
     })
